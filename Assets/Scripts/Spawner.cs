4707942c8e9cad419d6d34d2ae575f0566c70554
Weakmeak <ctibbett@student.neumont.edu>
2023-04-01 10:53:19 -0600

    Vehicle Test

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject obj;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) Instantiate(obj, transform.position, transform.rotation);
+    }
+}
